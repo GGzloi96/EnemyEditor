e4cdc57f9648ac058d852eaa7e03851c234d948e
GGzloi96 <kenackiken@yandex.ru>
2026-09-06 23:35:58 +0700

    Написал класс CEnemyTemplate и начал писать класс CEnemyTemplateList

---
+++ LibraryForLaba1/CEnemyTemplateList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryForLaba1
+{
+    internal class CEnemyTemplateList
+    {
+        List<CEnemyTemplate> enemies;
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+        
+        public void AddEnemy(string name, string iconName, int baseLife,
+                             double lifeModifier, int baseGold,
+                             double goldModifier, double spawnChance)
+        {
+            CEnemyTemplate newEnemy = new CEnemyTemplate(name,iconName,baseLife,lifeModifier,
+                                                         baseGold,goldModifier,spawnChance);
+            enemies.Add(newEnemy);
+        }
+
+        public CEnemyTemplate GetEnemyByName(string name)
+        {
+            
+            foreach (CEnemyTemplate enemy in enemies)
+            {
+                if (enemy.Name == name) return enemy;
+            }
+
+            return null;
+            
+        }
+
+        public CEnemyTemplate GetEnemyByIndex(int id)
+        {
+            if (enemies.Count != 0)
+            {
+                return enemies[id];
+            }
+            else { return null; }
+            
+        }
+
+        public void DeleteEnemyByName(string name)
+        {
+
+            foreach (CEnemyTemplate enemy in enemies)
+            {
+                if (enemy.Name == name) enemies.Remove(enemy);
+            }
+
+
+        }
+
+        public List<string> GetListOfEnemyNames()
+        {
+            if ( enemies.Count != 0)
+            {
+                List<string> list = new List<string>();
+                foreach(CEnemyTemplate enemy in enemies)
+                {
+                    list.Add(enemy.Name);
+                }
+                return list;
+            }
+            else { return null; }
+        }
+
+
+    }
+}
