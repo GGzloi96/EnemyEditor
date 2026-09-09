5fbf6247dc208a9a92a7a5a2a33348cab0459baa
GGzloi96 <kenackiken@yandex.ru>
2026-09-09 22:11:48 +0700

    Добавил кнопку для загручки иконок, собитие к кнопке и метод по открытию папки с фотками

--- EnemyEditor/MainEnemyEditor.xaml.cs
+++ EnemyEditor/MainEnemyEditor.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,16 +11,47 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace EnemyEditor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainEnemyEditor : Window
     {
+        private List<EnemyIcon> enemyIcons = new List<EnemyIcon>();
         public MainEnemyEditor()
         {
             InitializeComponent();
         }
+
+        private void LoadIconsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFolderDialog();
+            dialog.Title = "Выберите папку с иконками";
+
+            if (dialog.ShowDialog() == true)
+            {
+                string selectedPath = dialog.FolderName;
+                LoadIconsFromFolder(selectedPath);
+            }
+        }
+        private void LoadIconsFromFolder(string path)
+        {
+            enemyIcons.Clear();
+            string filter = "*.png";
+            string[] files = Directory.GetFiles(path, filter);
+
+
+            foreach (string file in files)
+            {
+                EnemyIcon icon = new EnemyIcon();
+
+                icon.Name = System.IO.Path.GetFileName(file);
+                icon.ImagePath = file;
+
+                enemyIcons.Add(icon);
+            }
+        }
+
+
     }
 }
